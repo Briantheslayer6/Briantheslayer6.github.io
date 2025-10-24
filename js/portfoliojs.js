8801f3aa6003da4c43fb77e6b5809cfdab0e2c87
Briantheslayer6 <161549048+Briantheslayer6@users.noreply.github.com>
2025-10-24 14:25:54 -0400

    Add files via upload

---
+++ js/portfoliojs.js
@@ -0,0 +1,9 @@
+let venom1 = document.getElementById("venom1");
+let goku1 = document.getElementById("goku1");
+let goku2 = document.getElementById("goku2");
+let combatgods3 = document.getElementById("combatgods3");
+
+combatgods3.onmouseover = function(){
+    combatgods3.style.height = "700px";
+    combatgods3.style.width = "700px";
+}
